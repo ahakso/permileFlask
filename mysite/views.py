6216e6a67be73f31888587a6eb77bd2e7dd3e850
Alex Hakso <alex.hakso@gmail.com>
2018-06-13 16:59:19 -0700

    Successfully base make selection back to views.py

--- mysite/views.py
+++ mysite/views.py
@@ -35,18 +35,9 @@ def cesareans_input():
 @app.route('/output')
 def cesareans_output():
   #pull 'birth_month' from input field and store it
-  patient = request.args.get('birth_month')
-    #just select the Cesareans  from the birth dtabase for the month that the user inputs
-  query = "SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean' AND birth_month='%s'" % patient
-  print(query)
-  query_results=pd.read_sql_query(query,con)
-  print(query_results)
-  births = []
-  for i in range(0,query_results.shape[0]):
-      births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
-      the_result = ''
-  the_result = ModelIt(patient,births)
-  return render_template("output.html", births = births, the_result = the_result)
+  patient = request.args.get('user_make')
+  print(patient)
+  return render_template("output.html", births = patient, the_result = patient)
 #@app.route('/output')
 #def cesareans_output():
 #    return render_template("output.html")
