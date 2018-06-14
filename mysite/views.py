191c166382dae0e0cb4c8c20aa22e41579b28d92
Alex Hakso <alex.hakso@gmail.com>
2018-06-14 15:56:46 -0700

    Successful population of the second dropdown menu

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,6 @@
 from flask import render_template
+from flask import jsonify
+from flask import url_for
 from mysite import app
 from mysite.a_Model import ModelIt
 from sqlalchemy import create_engine
@@ -20,7 +22,7 @@ def index():
     return render_template("index.html",
        title = 'Home', user = { 'nickname': 'Miguel' },
        )
-@app.route('/input')
+@app.route('/input', methods=['GET','POST'])
 def cesareans_input():
     return render_template("input.html", car_dict = car_dict)
 
@@ -30,6 +32,16 @@ def cesareans_output():
   user_make = request.args.get('user_make')
   print(user_make)
   return render_template("output.html", births = user_make, the_result = user_make)
+
+@app.route('/get_models')
+def get_models():
+    make = request.args.get('make')
+    if make:
+        models = list(car_dict[make])
+        data = [{"id": str(x), "name": str(x)} for x in models]
+        
+        print(data)
+    return jsonify(data)
 
 #@app.route('/get_model_array')
 #def read_txt_file():
